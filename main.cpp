0187127afe3c4c5816c9908e16b719591aa5b294
Jørn-Marcus Høylo-Rosenberg <jornmarh@uio.no>
2020-08-29 13:39:35 +0200

    fikset på algorytmen for å gi ut riktige verdier av u, lagt inn grensebetingelser på u

--- main.cpp
+++ main.cpp
@@ -12,97 +12,68 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
+  //create outfile
   ofstream outfile;
   outfile.open("data.txt");
-
   int n = atoi(argv[1]);
-  double* a; double* c; double* d;
-  a = new double[n-1]; c = new double[n-1]; d = new double[n];
-  for (int i = 0; i < n-1; i++) {
-    a[i] = rand() % 9 + 1;
-    c[i] = rand() % 9 + 1;
-    d[i] = rand() % 9 + 1;
-  }
-  d[n-1] = rand() % 9 + 1;
 
-  double h = 1.0/(n+1.0);
-  double* g;
-  g = new double[n];
-  for (int i = 0; i < n; i++) {
-    g[i] = h*h*100*exp(-10*i*h);
+  //initilize a,c,d,g,u,x
+
+  double* a; double* c; double* d;
+  a = new double[n+1]; c = new double[n+1]; d = new double[n+1];
+
+  double* x; double* u; double* g;
+  x = new double[n+1]; u = new double[n+1]; g = new double[n+1];
+
+  double h = 1.0/(n+1.0); //step length
+
+  a[0] = c[0] = d[0] = 0;
+
+  for (int i = 1; i <= n; i++) {
+    a[i] = c[i] = -1; //set values of a,c
+    d[i] = 2; //set values of d
+  }
+
+  for (int i = 0; i <= n; i++) {
+    x[i] = i*h; //set values of x
+    g[i] = h*h*100.0*exp(-10.0*x[i]); //set initial values of g.
+    //cout << x[i] << " " << g[i]  << " " << exp(-10.0*x[i]) << endl;
   }
   /*
-  cout << "a, c :" << endl;
-  for (int i = 0; i < n-1; i++) {
-    cout << a[i] << " " << c[i] << endl;
+  for (int i = 0; i <= n; i++) {
+    cout << d[i] << " " << g[i] << endl;
   }
-
-  cout << "g_start:" << endl;
-  for (int i = 0; i < n; i++) {
-    cout << g[i] << endl;
+  cout << " " << endl;
+*/
+  //forward substitution
+  for (int i = 2; i <= n; i++) {
+    d[i] = d[i] - ((a[i-1]*c[i-1])/d[i-1]);
+    g[i] = g[i] - ((a[i-1]*g[i-1])/d[i-1]);
+  }
+  /* print updated values
+  for (int i = 1; i <= n; i++) {
+    cout << d[i] << " " << g[i] << endl;
   }
   */
 
-  //forward substitution
-  for (int i = 1; i < n; i++) {
-    d[i] = d[i] - (a[i-1]*c[i-1])/d[i-1];
-    g[i] = g[i] - (a[i-1]*g[i-1])/d[i-1];
-  }
-
+  u[0] = u[n] = 0; //boundary conditions for solution u(x) x e(0,1)
   //backward substitution
-  double* u;
-  u = new double[n];
   u[n-1] = g[n-1]/d[n-1];
-  for (int i = n-1; i != 0 ; i--) {
-    u[i-1] = (g[i-1]- c[i-1]*u[i])/d[i-1];
-
+  for (int i = n-2; i > 0 ; i--) {
+    u[i] = (g[i]- c[i]*u[i+1])/d[i];
+  }
+  cout << "verdier for u[x(i)]" << endl;
+  for (int i = 0; i <= n; i++) {
+    cout << u[i] << " " << x[i] << endl;
   }
 
-  double* x;
-  x = new double[n];
+  //write u, x to outfile
+/*
   for (int i = 0; i < n; i++) {
-    x[i] = i*h;
-  }
-
-
-  //write to outfile
-  int rows = 2;
-  double** data;
-  data = new double*[rows];
-  for (int i = 0; i < rows; i++) {
-    data[i] = new double[n];
-  }
-  for (int j = 0; j < n; j++) {
-    data[0][j] = u[j];
-    data[1][j] = x[j];
-  }
-
-  for (int i = 0; i < n; i++) {
-    outfile << setprecision(8) << data[0][i] << " " << data[1][i] << endl;
+    outfile << setprecision(8) << x[i] << endl;
   }
   outfile.close();
-
-  /*
-  cout << "d_oppdatert:" << endl;
-  for (int i = 0; i < n; i++) {
-    cout << d[i] << endl;
-  }
-
-  cout << "g_oppdatert :" << endl;
-  for (int i = 0; i < n; i++) {
-    cout << g[i] << endl;
-  }
-  */
-
-
-
-
-
-
-
-
-
-
+*/
 
 
   delete[] a;
